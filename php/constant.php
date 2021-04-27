118439c47b20262a759c648fe381158435014ab9
Afzal MacBook Pro <afzal.attari786@gmail.com>
2021-04-27 11:17:28 +0500

    PHP-1

---
+++ php/constant.php
@@ -0,0 +1,18 @@
+<?php
+
+
+    //SQL Peramters
+    define("SQL_SERVER","localhost");
+    define("SQL_USER","root");
+    define("SQL_PASS","temp123");
+    define("SQL_DB","e-Rozgaar");
+
+    //Define HTML Break Line Constant
+    define("BR","<br />");
+
+    echo "SQL Server=".SQL_SERVER.BR;
+    echo "SQL Server UserName=".SQL_USER.BR;
+    echo "SQL Server Password=".SQL_PASS.BR;
+    echo "SQL Server DB=".SQL_DB.BR;
+
+?>
---
+++ php/es.php
@@ -0,0 +1,6 @@
+<?php
+
+    //Escape Sequence
+    echo "My Name is Muhammad Afzal \r\n And am i trainer at E-Rozgaar Project \r\n Here i am teching to Grad Students."
+
+?>
